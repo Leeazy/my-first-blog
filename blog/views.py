a3a65a6f7f243c74206b17d960bedfd5660b41bb
Leeazy <leeazy7@gmail.com>
2019-06-11 23:17:23 +0300

    template changes

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
+from .models import Post
 from django.shortcuts import render
+
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {"posts": posts})
+
 
 # Create your views here.
