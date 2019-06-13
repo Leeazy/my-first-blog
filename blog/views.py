3ce95a313834a2f316bb16c7bb7d876193d99b44
Leeazy <leeazy7@gmail.com>
2019-06-13 13:44:16 +0300

    Views and html

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from .models import Post
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order.by('published_date')
     return render(request, 'blog/post_list.html', {"posts": posts})
 
 
