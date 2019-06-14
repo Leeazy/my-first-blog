82659375be1c213e4a3ff3f89bb75d3fb3b17fb4
Leeazy <leeazy7@gmail.com>
2019-06-14 21:53:35 +0300

    set

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&)_!&_^&c035qvz$y30267*)*dkvx_zw-k2%(vct!$is21q%yo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'leeazy.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
